602f091a9ff324158df553a16f365e64dd4a81f2
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-05-11 15:18:30 +0300

    feat: add games-data and games-covers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { Switch, Route } from "react-router-dom";
 import { HomePage } from "./pages/home-page/";
 import { Header } from "./components/header";
 
+import { GAMES } from "./data";
+
 function App() {
+  console.log(GAMES);
   return (
     <div className="App">
       <div className="container">
